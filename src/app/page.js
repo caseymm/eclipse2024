b76bced6db691aa1070097628d94000c6250bb42
caseymm <caseymm@gmail.com>
2023-11-08 16:49:00 -0500

    got a moving circle

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+"use client";
+
 import Image from 'next/image'
 import * as d3 from 'd3'
 import { root } from 'mathjs'
 import styles from './page.module.css'
 import { Single_Day } from 'next/font/google';
+import overlap from './output.json';
+import { useEffect, useRef } from 'react';
+
 
 async function getData() {
   let url = 'https://aa.usno.navy.mil/api/eclipses/solar/date?date=2024-4-08 &coords=46.67, -122.65 &height=15';
@@ -79,33 +84,117 @@ export default async function Home() {
   const endVertexAngle = Math.round(parseFloat(end.vertex_angle));
   const midVertexAngle = (beginVertexAngle + endVertexAngle) / 2;
   // console.log(beginVertexAngle, endVertexAngle)
+  let lineData = [
+    [],
+    [],
+    []
+  ];
 
   const RenderCircle = ({svg}) => {
     let r = 100;
-    const Circles = () => {
+
+    const gRef = useRef();
+    let moon;
+    let moonPath;
+
+    useEffect(() => {
+      drawCircles();
+    }, []);
+
+    const drawCircles = () => {
+      const g = d3.select(gRef.current);
       // x = r * sin(-i),  y = r * cos(-i)
-      
-      return([...Array(360).keys()].map(i => {
+      [...Array(360).keys()].forEach(i => {
         let color = "";
-        const cx = r * Math.sin(-i);
-        const cy = r * Math.cos(-i);
+        let cx = r * Math.sin(-i);
+        let cy = r * Math.cos(-i);
         if(i === beginVertexAngle){
           color = "green";
           // generate start circle center
-          return <circle cx={cx*2} cy={cy*2} fill={color} r="2px"></circle>
+          cx = cx*2
+          cy = cy*2
+          lineData[0] = [cx, cy]
         }
         if(i === endVertexAngle){
           color = "red";
           // generate end circle center
-          return <circle cx={cx*2} cy={cy*2} fill={color} r="2px"></circle>
+          cx = cx*2
+          cy = cy*2
+          lineData[2] = [cx, cy]
         }
-        return <circle cx={cx} cy={cy} fill={color} r="2px"></circle>
-      }))
+        if(i === midVertexAngle){
+          const d = overlap[data.properties.obscuration.replace('%', '')]
+          color = "pink";
+          cx = cx*d
+          cy = cy*d
+          lineData[1] = [cx, cy]
+        }
+        if(color !== ""){
+          g.append('circle')
+           .attr('cx', cx)
+           .attr('cy', cy)
+           .attr('r', 2)
+           .attr('fill', color)
+        }
+      })
+      drawPaths();
     }
+
+    const drawPaths = () => {
+      const g = d3.select(gRef.current);
+      // Define the Bezier curve generator
+      const curve = d3.curveNatural; // You can choose different interpolation types
+
+      const line = d3.line()
+        .x(d => d[0])
+        .y(d => d[1])
+        .curve(curve); // Apply the Bezier curve to the line
+
+      // Add the Bezier curve path to the
+      moonPath = g.append('path')
+        .datum(lineData) // Bind the data
+        .attr('d', line) // Use the line generator
+        .attr('stroke', 'red')
+        .attr('fill', 'none');
+      
+      moon = g.append('circle')
+        // .attr('cx', lineData[0][0])
+        // .attr('cy', lineData[0][1])
+        .attr('r', 100)
+        .attr('fill', 'pink');
+      
+      animateCircle();
+    };
+
+    const animateCircle = () => {
+      // Select the circle element
+
+      // const circle = d3.select(circleRef.current);
+      // console.log(circle)
+  
+      // Use D3 transition to animate the circle along the path
+      moon.transition()
+        .duration(5000) // Duration of the animation (in milliseconds)
+        .attrTween('transform', translateAlongPath)
+        .on('end', animateCircle); // Restart the animation when it ends
+    };
+  
+    const translateAlongPath = () => {
+      return (t) => {
+        // Get the current position along the path
+        const path = moonPath.node();
+        const l = path.getTotalLength();
+        const p = path.getPointAtLength(t * l);
+        // console.log(path)
+  
+        // Translate the circle to the new position
+        return `translate(${p.x},${p.y})`;
+      };
+    };
+
     return(
-      <g transform="translate(300, 300)">
+      <g ref={gRef} transform="translate(300, 300)">
         <circle cx={'0px'} cy={'0px'} r={r} fill="blue"></circle>
-        <Circles/>
       </g>
       
     )
