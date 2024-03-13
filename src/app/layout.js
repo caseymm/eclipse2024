9b168bf7bc6b9a3dd8546e9385f420ebc8b4d68d
caseymm <caseymm@gmail.com>
2024-03-13 17:12:52 -0400

    ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Add custom scripts */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-HDXFDE0FLK`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'YOUR_TRACKING_ID');
+            `
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
